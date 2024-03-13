2cb1bc54a91286ee0351c74bff668251c92582dd
suhyeun <syg7708@naver.com>
2024-03-13 15:03:26 +0900

    header commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
-
+import "../css/header.css"
 
 export default function Header(){
+    return(
     <header>
-        <img src ="/image/Untitled.png"></img>
-        <div>
+        <img src ="/image/Untitled.png" width="100" height="100"></img>
+        <div className="headerlist">
             <ul>
                 <li>강아지</li>
                 <li>고양이</li>
                 <li>기타 동물</li>
             </ul>
         </div>
-        <div>
+        <div className="header_login">
             <ul>
                 <li>로그인</li>
                 <li>회원가입</li>
             </ul>
         </div>
      </header>
+    )
      
     
 }    
--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+import Header from "../component/Header";
+
 export default function HomePage(){
-    
+    return( 
+    <>
+    <Header></Header>
+    </>
+    )
 }
