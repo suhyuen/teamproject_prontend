81d09e24fd6080847afd9a2a496bf4322bd304b4
kimjaewoo0049 <41880358+kimjaewoo0049@users.noreply.github.com>
2024-03-14 12:55:06 +0900

    Jw/login4 (#6)
    * change loginpage -> signuppage
    
    * loginpage commit

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -3,54 +3,25 @@ import React, { useState } from "react"
 import '../css/LoginPage.css';
 
 export default function LoginPage(){
-    const [dropdownView, setDropdownView] = useState("listOff")
-
-    // dropDownButton 실행 이벤트
-    const handleClickDropDownButton = () => {
-        setDropdownView (dropdownView === "listOff" ? "listOn":"listOff");
-    }
-
-    // dropDownButton이 listOn상태일 때 바탕화면을 클릭하면 listOff로 바꿔주는 이벤트
-    const handleClickLoginContaner = () => {
-        if(dropdownView === "listOn"){
-            setDropdownView("listOff")
-        }
-    }
     
-      console.log(dropdownView)
-
-    return(
+    return (
         <>
-           <Header/>
-            <div className="lgoinContaner" onClick={handleClickLoginContaner}>
-                <div className="title"><h1>회원가입</h1></div>
-                <from className="content">
-                    <div>
-                        <input type="text" placeholder="이메일"></input>
-                        <button>중복체크</button>
-                    </div>
-                    <div>
-                        <input type="text" placeholder="닉네임"></input>
-                        <button>중복체크</button>
-                    </div>
-                    <input type="password" placeholder="비밀번호"></input>
-                    <input type="password" placeholder="비밀번호 확인"></input>
-                    <div className="dropDownButton">
-                        <div>
-                            <span onClick={handleClickDropDownButton}>키우고 있는 반려동물</span>
-                            <p  onClick={handleClickDropDownButton}>▼</p>
-                        </div>
-                        <ul className={dropdownView}>
-                            <li onClick={handleClickDropDownButton}>강아지</li>
-                            <li onClick={handleClickDropDownButton}>고양이</li>
-                            <li onClick={handleClickDropDownButton}>기타</li>
-                        </ul>
-                    </div>
-                    <input type="이름" placeholder="이름"></input>
-                    <input type="주소" placeholder="주소"></input>
-                </from>
-                <button className="button">회원가입</button>
+        <Header></Header>
+        <div className="loginContaner">
+            <img className="logo" src="../image/Untitled.png" alt="토끼그림"></img>
+            <form className="loginContent">
+                <input type="text" placeholder="아이디"></input>
+                <input type="text" placeholder="패스워드"></input>
+            </form>
+            <button className="loginButton">로그인</button>
+            <div className="loginUserFunc">
+                <div>
+                    <p>ID찾기</p>
+                    <p>비밀번호 찾기</p>
+                </div>
+                <p>회원가입</p>
             </div>
+        </div>
         </>
     )
 }
--- src/page/SignupPage.js
+++ src/page/SignupPage.js
@@ -1,3 +1,59 @@
+import Header from "../component/Header"
+import React, { useState } from "react"
+import '../css/SignupPage.css';
+
 export default function SignupPage(){
-    
+    const [dropdownView, setDropdownView] = useState("listOff")
+    const [dropDownText, setDropDownText] = useState("키우고 있는 반려동물")
+
+    // dropDownButton 실행 이벤트
+    const handleClickDropDownButton = (e) => {
+        setDropdownView (dropdownView === "listOff" ? "listOn":"listOff");
+        let text = e.target.dataset.value
+        if(text != null){
+            setDropDownText(text);
+        }
+    }
+
+    // dropDownButton이 listOn상태일 때 바탕화면을 클릭하면 listOff로 바꿔주는 이벤트
+    const handleClickLoginContaner = () => {
+        if(dropdownView === "listOn"){
+            setDropdownView("listOff")
+        }
+    }
+
+    return(
+        <>
+           <Header/>
+            <div className="lgoinContaner" onClick={handleClickLoginContaner}>
+                <div className="title"><h1>회원가입</h1></div>
+                <form className="content">
+                    <div>
+                        <input type="text" placeholder="이메일"></input>
+                        <button>중복체크</button>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="닉네임"></input>
+                        <button>중복체크</button>
+                    </div>
+                    <input type="password" placeholder="비밀번호"></input>
+                    <input type="password" placeholder="비밀번호 확인"></input>
+                    <div className="dropDownButton">
+                        <div>
+                            <span onClick={handleClickDropDownButton}>{dropDownText}</span>
+                            <p  onClick={handleClickDropDownButton}>▼</p>
+                        </div>
+                        <ul className={dropdownView}>
+                            <li className="dropdownText" data-value="강아지" onClick={handleClickDropDownButton}>강아지</li>
+                            <li className="dropdownText" data-value="고양이" onClick={handleClickDropDownButton}>고양이</li>
+                            <li className="dropdownText" data-value="기타" onClick={handleClickDropDownButton}>기타</li>
+                        </ul>
+                    </div>
+                    <input type="이름" placeholder="이름"></input>
+                    <input type="주소" placeholder="주소"></input>
+                </form>
+                <button className="button">회원가입</button>
+            </div>
+        </>
+    )
 }
