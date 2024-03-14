744e38c596d0f434a9ab023b780ccc936b0ea868
Song Jungeun <jungeun2474@gmail.com>
2024-03-14 10:54:13 +0900

    회원 탈퇴 페이지 구현

--- src/page/UserdeletePage.js
+++ src/page/UserdeletePage.js
@@ -1,3 +1,44 @@
-export default function UserdeletePage(){
-    
+import "../css/userdeletepage.css";
+import Header from "../component/Header.js";
+
+export default function UserdeletePage() {
+  return (
+    <div>
+      <div id="userdeletemain">
+        <Header />
+        <div id="userdelete-content">
+          <img id="userdelete-img" src="/image/userimg.png" alt="userimg"></img>
+          <h1 id="userdelete-title">회원 탈퇴</h1>
+          <form id="userdeletle-form">
+            <div id="userdelete-conditions">
+              가입된 회원정보가 모두 삭제됩니다. 작성하신 게시물은 삭제되지
+              않습니다.
+              <br /> 탈퇴 후 같은 계정으로 재가입 시 기존에 작성하신 게시물에
+              대한 권한은 없어집니다. <br />
+              회원 탈퇴에 동의하십니까?
+            </div>
+            <div id="userdelete-agree">
+              <input type="checkbox" id="userdelete-agreebox" />
+              <label id="userdelete-agreelabel" for="agree">
+                동의합니다.
+              </label>
+            </div>
+            <input
+              id="userdelete-pw"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+            ></input>
+            <button id="userdelete-button" type="submit">
+              <img
+                id="userdelete-buttonimg"
+                src="/image/pawbutton.png"
+                alt="paw"
+              ></img>
+              탈퇴
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
