9f9a7a96c2f7163cea90b81800e306aa51a3653f
suhyuen <162869193+suhyuen@users.noreply.github.com>
2024-03-15 10:25:12 +0900

    updateuser_commit (#11)

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
 
-import "../css/header.css"
+import "../css/header.css";
 
-export default function Header(){
-    return(
+export default function Header() {
+  return (
     <header>
-        <img src ="/image/Untitled.png" width="100" height="100"></img>
-        <div className="headerlist">
-            <ul>
-                <li>강아지</li>
-                <li>고양이</li>
-                <li>기타 동물</li>
-            </ul>
-        </div>
-        <div className="header_login">
-            <ul>
-                <li>로그인</li>
-                <li>회원가입</li>
-            </ul>
-        </div>
-     </header>
-    )
-     
-    
-}    
+      <Link to="/">
+        <img src="/image/Untitled.png" width="100" height="100"></img>
+      </Link>
+      <div className="headerlist">
+        <ul>
+          <Link to="/dogposts">
+            <li>강아지</li>
+          </Link>
+          <Link to="/catposts">
+            <li>고양이</li>
+          </Link>
+          <Link to="/etcposts">
+            <li>기타 동물</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="header_login">
+        <ul>
+          <Link to="/login">
+            <li className="link_login">로그인</li>
+          </Link>
+          <Link to="/signup">
+            <li>회원가입</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
--- src/page/UpdateuserPage.js
+++ src/page/UpdateuserPage.js
@@ -1,3 +1,69 @@
-export default function UpdateuserPage(){
-    
+import { Link } from "react-router-dom";
+import "../css/updateuser.css";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import { useState, useEffect } from "react";
+
+export default function UpdateuserPage() {
+  const [userData, setUserData] = useState({
+    userName: "홍길동",
+    serNickname: "홍길동",
+    userId: "id",
+    userEmail: "email",
+    userAdress: "adress",
+  });
+  return (
+    <>
+      <Header></Header>
+      <form>
+        <nav className="updateuserpage">
+          <div>회원 정보 수정</div>
+          <div className="updateuser_1">
+            <img src="/image/강아지2.jpg" width="210px" height="151px"></img>
+            <button>사진 추가</button>
+            <div>
+              <div className="mypage_list">
+                <p>이름</p>
+                <div>{userData.userName}</div>
+              </div>
+              <div className="mypage_list">
+                <p>닉네임</p>
+                <input type="text"></input>
+                <button>중복확인</button>
+              </div>
+              <div className="mypage_list">
+                <p>아이디</p>
+                <input type="text"></input>
+              </div>
+              <div className="mypage_list">
+                <p>이메일</p>
+                <input type="text"></input>
+              </div>
+              <div className="mypage_list">
+                <p>주소</p>
+                <input type="text"></input>
+              </div>
+            </div>
+            <div className="updateuser_button">
+              <button>
+                <img
+                  src="/image/강아지_발바닥-removebg-preview.png"
+                  width="47px"
+                  height="47px"
+                ></img>
+                <p>수정하기</p>
+              </button>
+            </div>
+            <div className="updateuser_end">
+              <Link to="/userdelete">
+                <div>회원탈퇴</div>
+              </Link>
+              <div>회원 탈퇴 시 계정 관련 데이터를 복구할 수 없습니다.</div>
+            </div>
+          </div>
+        </nav>
+      </form>
+      <Footer></Footer>
+    </>
+  );
 }
