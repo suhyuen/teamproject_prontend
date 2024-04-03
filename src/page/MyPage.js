adcdb21b4437a15cd6bb1dacb12c39aed1d0c1f9
kimjaewoo0049 <41880358+kimjaewoo0049@users.noreply.github.com>
2024-04-03 12:10:46 +0900

    Update MyPage.js

--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -25,7 +25,6 @@ export default function MyPage() {
   });
 
   useEffect(() => {
-<<<<<<< HEAD
     const response = axios.get("http://localhost:8080/userInfo", 
     {
       params: { userId: userIdSelecter },
